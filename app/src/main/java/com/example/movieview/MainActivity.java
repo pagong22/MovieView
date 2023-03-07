b7f6bad88fff742d0c205a5517e9d451580a10ed
pagong22 <124701523+pagong22@users.noreply.github.com>
2023-03-07 15:17:40 +0000

    Pushed changes
    completed skeletal structure based on figma design

--- app/src/main/java/com/example/movieview/MainActivity.java
+++ app/src/main/java/com/example/movieview/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.movieview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.accountbtn);
+        button = findViewById(R.id.homebtn);
+        button = findViewById(R.id.reviewbutton);
+        button = findViewById(R.id.searchbtn);
     }
 }
